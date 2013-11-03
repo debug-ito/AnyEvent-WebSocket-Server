2f4a2aedd3ec0bcc86a58d41279991489129df08
Toshio Ito <debug.ito@gmail.com>
2013-11-03 17:16:08 +0900

    CPAN release version 0.02

--- lib/AnyEvent/WebSocket/Server.pm
+++ lib/AnyEvent/WebSocket/Server.pm
@@ -7,7 +7,7 @@ use Protocol::WebSocket::Handshake::Server;
 use Try::Tiny;
 use AnyEvent::WebSocket::Connection;
 
-our $VERSION = "0.01";
+our $VERSION = "0.02";
 
 sub new {
     my ($class, %args) = @_;
