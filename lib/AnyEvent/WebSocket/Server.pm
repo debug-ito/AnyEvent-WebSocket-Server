069294b034158b66b52ad07074f2f3a6bd82a97e
Toshio Ito <debug.ito@gmail.com>
2014-08-17 11:45:27 +0900

    Release 0.05

--- lib/AnyEvent/WebSocket/Server.pm
+++ lib/AnyEvent/WebSocket/Server.pm
@@ -7,7 +7,7 @@ use Protocol::WebSocket::Handshake::Server;
 use Try::Tiny;
 use AnyEvent::WebSocket::Connection;
 
-our $VERSION = "0.04";
+our $VERSION = "0.05";
 
 sub new {
     my ($class, %args) = @_;
